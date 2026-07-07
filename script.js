0c18322c76684e4b9ddc5cd9bf387100f5dca2b8
inf0chan <infochans11@gmail.com>
2026-07-07 17:50:43 +0530

    Updated index.html, style.css and script.js

--- script.js
+++ script.js
@@ -1,2 +1,38 @@
-document.querySelector("current-year").textContent = new Date().getFullYear();
+document.querySelector("#current-year").textContent = new Date().getFullYear();
+const loginBtn = document.getElementById("loginBtn");
+const loginDialog = document.getElementById("loginDialog");
+
+const signupDialog = document.getElementById("signupDialog");
+const showSignup = document.getElementById("showSignup");
+const showLogin = document.getElementById("showLogin");
+
+document.querySelectorAll("form").forEach(form => {
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+    });
+});
+
+loginBtn.addEventListener("click", () => {
+    loginDialog.showModal();
+});
+
+showSignup.addEventListener("click", (e) => {
+    e.preventDefault();
+    loginDialog.close();
+    signupDialog.showModal();
+});
+
+showLogin.addEventListener("click", (e) => {
+    e.preventDefault();
+    signupDialog.close();
+    loginDialog.showModal();
+});
+
+document.getElementById("closeLogin").onclick = () => {
+    loginDialog.close();
+};
+
+document.getElementById("closeSignup").onclick = () => {
+    signupDialog.close();
+};
 
